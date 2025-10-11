a82a3010eb09121fb17474130928c7f525d88419
Paulo Soares <paulinho-freitas@live.com>
2025-10-11 15:17:54 -0300

    Correção na URL da API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000'
+    baseURL: 'https://projeto-back-git-main-devpaulos-projects-6099a8f9.vercel.app/'
 })
 
 export default api
