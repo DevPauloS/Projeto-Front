206d74968354c2916edd79a13d23d148b94cc29f
Paulo Soares <paulinho-freitas@live.com>
2026-02-19 22:39:26 -0300

    Inserindo opção de verificar a lista de usuarios cadastrados para testar retorno do BD

--- src/pages/Inicial/index.jsx
+++ src/pages/Inicial/index.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router-dom";
+
 export default function Inicial() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-center text-2xl font-bold size- text-blue-900">
+    <div className="flex flex-col justify-around items-center h-screen">
+      <h1 className="text-7xl font-bold text-blue-900">
         PAGINA INICIAL EM ELABORAÇÃO!
       </h1>
+      <Link to="/listar-usuarios" className="text-6xl text-pink-600 font-bold">
+        Listar Usuários
+      </Link>
     </div>
   );
 }
